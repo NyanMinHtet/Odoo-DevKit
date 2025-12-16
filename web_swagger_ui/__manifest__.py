f419c57472751cf1ce79a623b84fce78404148f9
NyanMinHtet <nyanminhtet178004@gmail.com>
2025-12-16 23:44:00 +0630

    [FIX] fix to compactible with odoo app store rule

--- web_swagger_ui/__manifest__.py
+++ web_swagger_ui/__manifest__.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Swagger UI',
-    'summary': 'Integrates Swagger UI for API documentation.',
     'version': '18.0.1.0.0',
     'category': 'Tools',
     'author': 'Shoto',
     'license': 'LGPL-3',
+    'description': """
+        This module integrates Swagger UI into Odoo to provide a user-friendly interface for API documentation.
+    """,
     'depends': ['base', 'web'],
     'data': [
         'views/swagger_ui_template.xml',
     ],
+    'images': ['static/description/icon.png'],
     'installable': True,
     'application': True,
 }
